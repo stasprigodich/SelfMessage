03fdceb05dc460cb1f15d638de35ebb4e21b2839
Stanislav Prigodich <stas.prigodich@gmail.com>
2015-11-04 15:40:34 +0300

    Initial Commit

---
+++ self-message/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  self-message
+//
+//  Created by Stanislav Prigodich on 04/11/15.
+//  Copyright © 2015 prigodich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
